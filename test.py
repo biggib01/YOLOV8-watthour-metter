e555d7a143d591a6de52c24ed83580a8b3605d56
CVX63 <supachok8728@gmail.com>
2024-10-03 12:24:24 +0700

    Update test function

--- test.py
+++ test.py
@@ -1,11 +1,6 @@
 from ultralytics import YOLO
 import torch
 
-mps_device = torch.device("mps")
-
-print(torch.backends.mps.is_built())
-
-print(torch.backends.mps.is_available())
 # Load a model
 model = YOLO('yolov8s.pt')  # load a pretrained model (recommended for training)
 # Train the model with 2 GPUs
